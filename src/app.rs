e1ac2816e667722661f77a63f958e197c1146f9f
lpnh <lipe.paniguel@gmail.com>
2024-02-05 16:08:33 -0300

    add small improvements

--- src/app.rs
+++ src/app.rs
@@ -29,38 +29,38 @@ fn Home() -> impl IntoView {
             </div>
             // Main Content
             <div
-                class="flex flex-grow mb-auto justify-center items-center"
+                class="flex flex-grow mb-auto justify-start items-center"
             >
-                <div class="flex md:flex-row flex-col justify-center items-center md:items-end md:space-x-12 space-x-0 md:space-y-0 space-y-12 transition-colors duration-200">
+                <div class="flex flex-col justify-start md:space-y-12 space-y-8 transition-colors duration-200">
                     <a
-                        class="flex flex-col items-center space-y-4 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
-                        href="/blog"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 512 512">
-                            <path fill="currentColor" d="M131.3 20.35c-14.6.1-28.1 10-31.93 24.82c-2.33 9.13-.55 18.4 4.13 25.84c-7.67 4.26-13.69 11.53-16.03 20.66c-2.32 9.13-.56 18.33 4.1 25.83a32.687 32.687 0 0 0-15.96 20.6c-2.34 9.1-.54 18.4 4.18 25.8c-7.72 4.3-13.75 11.5-16.09 20.7c-2.33 9.1-.54 18.4 4.19 25.8c-7.72 4.3-13.75 11.5-16.09 20.7c-2.34 9.1-.54 18.4 4.18 25.8c-7.72 4.3-13.75 11.5-16.08 20.7c-2.34 9.1-.54 18.4 4.18 25.8c-7.72 4.3-13.75 11.5-16.09 20.7c-2.35 9.2-.51 18.5 4.3 26a32.915 32.915 0 0 0-16.28 20.8c-4.48 17.5 6.25 35.6 23.79 40.1l.1-.2l31.71 8.2l-1.47 5.7l261.56 67L374 326.5l-22.4 21.2l-87.8 26.5l15.5-42.5l-151.7-38.8l4.4-17.4l153.5 39.3l9.7-26.7l15.3-14.4l-167-42.8l4.4-17.4l178 45.6l39.6-37.4l-206.1-52.8l4.4-17.4L380.7 207l-.1.4l31.5-29.8l18.3-71.4l-261.6-67.04l-4.8 18.66c2.2-16.32-8.1-32.27-24.5-36.44c-2.7-.7-5.5-1.04-8.2-1.03zm.3 17.99c1.2 0 2.4.19 3.5.48c8.1 2.09 12.9 10.13 10.8 18.27l17.2 4.4l-11 42.81c2.2-16.35-8.2-32.26-24.5-36.43l-.6-.15c-7.8-2.34-12.2-10.15-10.2-18.07c1.7-6.61 7.3-11 13.7-11.3h1.1zm-11.9 46.51c.9 0 1.9.14 2.9.36l.6.15c8.1 2.08 12.9 10.12 10.8 18.24l17.2 4.4l-11 43c2.4-16.4-8-32.6-24.4-36.7c-.7-.2-1.3-.4-1.9-.5c-7-2.7-10.9-10.1-9-17.62c1.7-6.97 7.9-11.45 14.8-11.29zm59.9 4.59l217 55.66l-4.4 17.4l-217-55.6zm-72.9 41.86h1.3c.5 0 .9 0 1.4.1c.6.2 1.2.3 1.8.5l.1-.2c8.1 2.1 12.9 10.1 10.8 18.3l17.2 4.4l-11 43c2.3-16.3-8.1-32.4-24.4-36.6c-8.18-2.1-12.94-10.1-10.85-18.3c1.69-6.6 7.25-10.9 13.65-11.2zM465.4 152l-10.2 9.6l31.6 33.5l10.2-9.6zm-23.3 22L315.7 293.5l31.5 33.5l126.5-119.5zm-347.23 3.7c1.48 0 3 .1 4.53.5c8.1 2.1 12.9 10.1 10.8 18.3l17.2 4.4l-11 43c2.3-16.4-8.1-32.4-24.44-36.6c-8.14-2.1-12.9-10.1-10.82-18.3c1.7-6.6 7.32-11 13.73-11.3zm-11.91 46.5c1.48 0 3 .1 4.53.5c8.14 2.1 12.91 10.1 10.81 18.3l17.2 4.4l-11 42.9c2.3-16.3-8.1-32.3-24.45-36.5c-8.14-2.1-12.89-10.1-10.81-18.3c1.69-6.6 7.31-11 13.72-11.3zm-11.9 46.5c1.48 0 3 .1 4.53.5c8.13 2.1 12.89 10.1 10.81 18.3l17.2 4.3l-10.94 42.8c2.16-16.3-8.25-32.1-24.51-36.3c-8.14-2.1-12.9-10.1-10.82-18.3c1.7-6.6 7.32-11 13.73-11.3zm235.34 39.2L293 346.6l37.4-11.3zm-247.25 7.3c1.48 0 3 .1 4.53.5c8.14 2.1 12.9 10.1 10.81 18.3l17.21 4.3l-11 43c2.1-16.2-8.3-32-24.53-36.2l.1-.3c-8.16-2.1-12.92-10.1-10.84-18.3c1.69-6.6 7.31-11 13.72-11.3zm56.95 20.3L333.2 393l-4.4 17.4l-217.1-55.5zM47.18 364c1.48 0 3 .1 4.52.5c8.14 2.1 12.9 10.1 10.82 18.3l17.2 4.3l-3.69 14.4l-31.92-8.2v.2c-8.01-2.2-12.67-10.1-10.61-18.2c1.7-6.6 7.32-11 13.73-11.3z"/>
-                        </svg>
-                        <div class="md:space-y-2 space-y-1">
-                            <p class="tracking-wide md:w-64 md:text-3xl text-2xl">"Blog"</p>
-                            <p>"random stuff"</p>
-                        </div>
-                    </a>
-                    <a
-                        class="flex flex-col items-center space-y-4 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
+                        class="flex flex-row items-center md:space-x-8 space-x-6 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
                         href="https://veredas.shuttleapp.rs/"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
                             <path fill="teal" d="M16.5 21c-3 0-4.19-4.24-5.45-8.72C10.14 9.04 9 5 7.5 5C4.11 5 4 11.93 4 12H2c0-.37.06-9 5.5-9c3 0 4.21 4.25 5.47 8.74C13.83 14.8 15 19 16.5 19c3.44 0 3.53-6.93 3.53-7h2c0 .37-.06 9-5.53 9"/>
                         </svg>
-                        <div class="md:space-y-2 space-y-1">
-                            <p class="tracking-wide md:w-64 md:text-3xl text-2xl">"Veredas"</p>
-                            <p>"notpron-like game"</p>
+                        <div class="flex flex-col items-start space-y-1">
+                            <p class="tracking-wide md:text-2xl text-xl">"Veredas"</p>
+                            <p class="text-start md:w-full w-48">"a notpron-like game"</p>
                         </div>
                     </a>
                     <a
-                        class="flex flex-col items-center space-y-4 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
+                        class="flex flex-row items-center md:space-x-8 space-x-6 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
+                        href="/mono-no-aware"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 36 36">
+                            <path fill="#F4ABBA" d="M31.298 20.807c4.197-1.363 5.027-3.182 4.191-6.416c-.952.308-2.105-.001-2.272-.518c-.168-.513.581-1.443 1.533-1.753c-1.223-3.107-2.964-4.089-7.161-2.727c-1.606.522-3.238 1.492-4.655 2.635C23.582 10.327 24 8.475 24 6.786c0-4.412-1.473-5.765-4.807-5.968c0 1-.652 2-1.193 2s-1.194-1-1.194-2C13.472 1.021 12 2.374 12 6.786c0 1.689.417 3.541 1.066 5.241c-1.416-1.142-3.049-2.111-4.655-2.633c-4.197-1.364-5.938-.381-7.162 2.727c.951.31 1.701 1.238 1.534 1.753c-.167.515-1.32.826-2.271.518c-.837 3.233-.005 5.052 4.19 6.415c1.606.521 3.497.697 5.314.605c-1.524.994-2.95 2.247-3.943 3.613c-2.594 3.57-2.197 5.53.381 7.654c.588-.809 1.703-1.235 2.142-.917c.438.317.378 1.511-.21 2.32c2.816 1.795 4.803 1.565 7.396-2.003c.993-1.366 1.743-3.111 2.218-4.867c.475 1.757 1.226 3.501 2.218 4.867c2.594 3.57 4.58 3.798 7.397 2.003c-.587-.81-.649-2.002-.21-2.321c.437-.317 1.553.107 2.142.917c2.577-2.123 2.973-4.083.381-7.653c-.993-1.366-2.42-2.619-3.943-3.613c1.816.092 3.706-.084 5.313-.605M18 20.337c-.162-.292-.353-.538-.588-.709c-.234-.171-.528-.276-.856-.341c.228-.244.403-.502.493-.778c.09-.275.1-.587.059-.919c.302.141.602.228.892.228s.59-.087.894-.229c-.041.332-.031.644.059.919c.09.276.265.534.492.778c-.327.065-.621.17-.855.341c-.236.172-.428.418-.59.71"/><g fill="#FFF"><path d="M16.795 18.685a1 1 0 0 1-1.193.76l-5.858-1.302a1 1 0 1 1 .434-1.952l5.857 1.302c.54.12.88.653.76 1.192m.976.218a1 1 0 0 1 1.193-.76l5.857 1.302a1 1 0 1 1-.433 1.952l-5.857-1.302a.998.998 0 0 1-.76-1.192"/><path d="M17.771 18.903a1 1 0 0 1-1.38-.308l-3.221-5.061a.999.999 0 1 1 1.687-1.073l3.222 5.062a1.001 1.001 0 0 1-.308 1.38m.797 1.252a1 1 0 0 1 1.38.307l3.223 5.062a1 1 0 0 1-1.689 1.074l-3.221-5.062a1.003 1.003 0 0 1 .307-1.381"/><path d="M17.988 17.927a1 1 0 0 1-.759-1.193l1.302-5.857a1 1 0 1 1 1.951.434l-1.301 5.857a1 1 0 0 1-1.193.759m-.217.976a.998.998 0 0 1 .759 1.192l-1.3 5.857a1 1 0 0 1-1.954-.433l1.303-5.857a.998.998 0 0 1 1.192-.759"/><path d="M17.771 18.902a.999.999 0 0 1 .307-1.38l5.062-3.221a.999.999 0 1 1 1.073 1.687l-5.062 3.221a.998.998 0 0 1-1.38-.307"/><path d="M17.771 18.902a1 1 0 0 1-.307 1.381l-5.062 3.222a1 1 0 1 1-1.074-1.688l5.062-3.222a1.002 1.002 0 0 1 1.381.307"/></g><path fill="#EA596E" d="M22.012 25.566a2.001 2.001 0 1 1 2.146 3.376a2.001 2.001 0 0 1-2.146-3.376M14.55 13.841a2 2 0 1 1-2.148-3.373a2 2 0 0 1 2.148 3.373m1.92 10.918a2.002 2.002 0 1 1-.867 3.906a2.002 2.002 0 0 1 .867-3.906m3.037-13.665a2 2 0 1 1 .868-3.905a2 2 0 1 1-.868 3.905m-6.013 10.53a2 2 0 1 1-3.375 2.15a2 2 0 0 1 3.375-2.15m9.339-5.943a2 2 0 1 1 3.377-2.147a2 2 0 0 1-3.377 2.147m.795 4.524a2.002 2.002 0 0 1 2.387-1.52a2 2 0 1 1-2.387 1.52m-12.69-2.821a2 2 0 1 1-3.905-.868a2 2 0 0 1 3.905.868"/><circle cx="18" cy="18.818" r="4" fill="#FFCC4D"/>
+                        </svg>
+                        <div class="flex flex-col items-start space-y-1">
+                            <p class="tracking-wide md:text-2xl text-xl">"Mono no aware"</p>
+                            <p class="text-start md:w-full w-48">"small doses of random writings"</p>
+                        </div>
+                    </a>
+                    <a
+                        class="flex flex-row items-center md:space-x-8 space-x-6 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200"
                         href="/why-rust"
                     >
-                        <svg width="80" height="80" viewBox="0 0 4417 3259" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
+                        <svg width="48" height="48" viewBox="0 0 4417 3259" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
                             <g transform="matrix(4.16667,0,0,4.16667,0,0)">
                                 <path d="M525.403,293.05C393.77,293.05 274.175,308.875 185.633,334.665L185.633,554.963C274.175,580.753 393.77,596.577 525.403,596.577C676.06,596.577 810.938,575.848 901.537,543.175L901.537,346.457C810.938,313.781 676.06,293.05 525.403,293.05Z" style="fill:rgb(143,30,28);fill-rule:nonzero;"/>
                                 <path d="M907.423,492.442C903.566,481.779 902.794,468.288 906.062,455.28C911.912,431.991 928.483,419.082 943.075,426.447C946.693,428.274 949.849,431.178 952.462,434.865C952.701,434.864 952.94,434.865 953.177,434.881C953.177,434.881 997.729,487.987 956.49,550.884C955.595,554.453 879.956,642.602 862.447,645.408C850.987,647.244 877.338,555.41 907.423,492.442Z" style="fill:rgb(143,30,28);fill-rule:nonzero;"/>
@@ -79,29 +79,29 @@ fn Home() -> impl IntoView {
                                 <path d="M404.746,695.984C404.746,695.984 459.949,703.279 535.416,705.14C542.026,657.629 506.036,607.348 448.615,587.897C385.177,566.409 319.626,590.689 302.201,642.129C284.776,693.569 322.077,752.689 385.515,774.178C413.636,783.704 442.168,784.227 466.744,777.385C429.833,740.88 404.746,695.984 404.746,695.984Z" style="fill:rgb(228,58,37);fill-rule:nonzero;"/>
                             </g>
                         </svg>
-                        <div class="md:space-y-2 space-y-1">
-                            <p class="tracking-wide md:w-64 md:text-3xl text-2xl">"Why Rust"</p>
-                            <p>"my two cents"</p>
+                        <div class="flex flex-col items-start space-y-1">
+                            <p class="tracking-wide md:text-2xl text-xl">"Why Rust"</p>
+                            <p class="text-start md:w-full w-48">"some thoughts from my journey"</p>
                         </div>
                     </a>
                 </div>
             </div>
             // Other socials
-            <div class="md:mb-16 mb-10 md:w-96">
+            <div class="md:mb-16 mb-10">
                 <h3 class="md:pb-6 pb-4 md:text-lg text-base">"Find me on:"</h3>
-                <div class="flex flex-row justify-around items-center space-x-6">
+                <div class="flex flex-row items-center space-x-8">
                     <a href="https://github.com/lpnh" target="_blank" class="justify-center items-center flex flex-col space-y-2 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/>
                         </svg>
                         <p class="md:text-base text-sm">"github"</p>
                     </a>
-                    <a href="https://www.linkedin.com/in/lpnh/" target="_blank" class="justify-center items-center flex flex-col space-y-2 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200">
+                    /* <a href="https://www.linkedin.com/in/lpnh/" target="_blank" class="justify-center items-center flex flex-col space-y-2 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
                             <path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77Z"/>
                         </svg>
                         <p class="md:text-base text-sm">"linkedin"</p>
-                    </a>
+                    </a> */
                     <a href="https://discordapp.com/users/371617418285416448" target="_blank" class="justify-center items-center flex flex-col space-y-2 md:hover:text-rose-300 active:text-rose-300 transition-colors duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
                             <path fill="currentColor" d="m22 24l-5.25-5l.63 2H4.5A2.5 2.5 0 0 1 2 18.5v-15A2.5 2.5 0 0 1 4.5 1h15A2.5 2.5 0 0 1 22 3.5V24M12 6.8c-2.68 0-4.56 1.15-4.56 1.15c1.03-.92 2.83-1.45 2.83-1.45l-.17-.17c-1.69.03-3.22 1.2-3.22 1.2c-1.72 3.59-1.61 6.69-1.61 6.69c1.4 1.81 3.48 1.68 3.48 1.68l.71-.9c-1.25-.27-2.04-1.38-2.04-1.38S9.3 14.9 12 14.9s4.58-1.28 4.58-1.28s-.79 1.11-2.04 1.38l.71.9s2.08.13 3.48-1.68c0 0 .11-3.1-1.61-6.69c0 0-1.53-1.17-3.22-1.2l-.17.17s1.8.53 2.83 1.45c0 0-1.88-1.15-4.56-1.15m-2.07 3.79c.65 0 1.18.57 1.17 1.27c0 .69-.52 1.27-1.17 1.27c-.64 0-1.16-.58-1.16-1.27c0-.7.51-1.27 1.16-1.27m4.17 0c.65 0 1.17.57 1.17 1.27c0 .69-.52 1.27-1.17 1.27c-.64 0-1.16-.58-1.16-1.27c0-.7.51-1.27 1.16-1.27Z"/>
